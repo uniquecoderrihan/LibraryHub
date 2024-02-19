9030ffdeeca1f18bdcda9ad12fae1310329475d7
raihanuldev <rmmahi4770@gmail.com>
2024-02-19 15:14:38 +0600

    defined nabvar & footer area

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <main>
+    <p>This is Nabvar</p>
+    <Component {...pageProps} />
+    <p>This is FOoter</p>
+  </main>;
 }
---
+++ src/pages/books.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const books = () => {
+    return (
+        <div>
+            this is book routes
+        </div>
+    );
+};
+
+export default books;
