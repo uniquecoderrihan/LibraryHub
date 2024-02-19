a079a50dc8d788b53c83a1d7b41b88ad9158e2fb
raihanuldev <rmmahi4770@gmail.com>
2024-02-19 16:27:00 +0600

    Banner with Search Bar

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://s26162.pcdn.co/wp-content/uploads/2023/06/overflowing-bookcases.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md flex">
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs"  />
+                    <button className='btn'>Search</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Banner from "@/components/banner/Banner";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,12 +8,7 @@ export default function Home() {
     <main
       className={`${inter.className}`}
     >
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <input type="checkbox" className="toggle" checked />
+      <Banner/>
     </main>
   );
 }
